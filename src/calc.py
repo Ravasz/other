777f657b147e94c85426f527e8752a336cfb8a8c
Ravasz <rav.mate@gmail.com>
2023-04-27 23:26:06 +0100

    added script to test IDE

--- src/calc.py
+++ src/calc.py
@@ -2,3 +2,22 @@
 
 # testing it again
 
+# test IDE with the code below:
+from random import randint
+init_bet = 100
+max_bet = 800
+pool = 0
+
+n = 4
+for j in range(100):
+    for i in range(1, n + 1):
+        cur_bet = (2**(i-1)) * init_bet
+        roll = randint(0,36)
+        if 0 < roll < 19: 
+            pool += cur_bet
+            # print(pool)
+            break
+        else: 
+            pool -= cur_bet
+    print(pool)
+
