54f5ce4837d27b8b631b39f6d91ab2f1f9265868
Ravasz <rav.mate@gmail.com>
2025-05-27 12:50:44 +0100

    added another exercise

--- kinase_test.R
+++ kinase_test.R
@@ -68,51 +68,154 @@ print(paste0(
 ### phosphopeptide test ########################################################
 
 prot_seq <- "PVMHQRDQHFVERMPKPRGQPFTQYLLHDLQIQKVMISGGKKPMNECCWWRQPVNFDFSADKQAIYCVDIAKIDMQQDKWMHRQSISGTSLKMAAWNLGYHKISAFRVMHNKCPMANWMMIYMTLPRPTKQWSRFVKRQDLEYFQYCMFYQAILDTLLPIRYCNTNTCRKCEKMPKFTYQREGCRPDNIHASKYEFIG"
+prot_seq_mod <- "PVS*HQRDQHT*FVERMPKPRGQPFT*QYLLHDLQIQKVMISGGKKPMNECCWWRQPVNFDFSADKQAIYCVDIAKIDMQQDKWMHRQSISGTSLKMAAWNLGYHKISAFRVMHNKCPMANWMMIYMTLPRPTKQWSRFVKRQDLEYFQYCMFYQAILDTLLPIRYCNTNTCRKCEKMPKFTY*QREGCRPDNIHASKYEFIG"
 
-tryptic_cut <- function(seq_in) {
+trypsin_digest <- function(seq_in) {
 
-  peptides <- c()
-  current_peptide_start <- 1
-  seq_length <- nchar(seq_in)
+    peptides <- c()
+    current_peptide_start <- 1
+    seq_length <- nchar(seq_in)
 
-  # Iterate through the protein sequence character by character
-  for (i in 1:seq_length) {
-    # Get the current amino acid
-    current_aa <- substr(seq_in, i, i)
+    # Iterate through the protein sequence character by character
+    for (i in 1:seq_length) {
+        current_aa <- substr(seq_in, i, i)
+        if (current_aa == "*") next
+        if (current_aa == "K" || current_aa == "R") {
+            # Check if it's not the last amino acid in the sequence
+            if (i < seq_length) {
+                next_aa <- substr(seq_in, i + 1, i + 1)
 
-    # Check for Lysine (K) or Arginine (R)
-    if (current_aa == "K" || current_aa == "R") {
-      # Check if it's not the last amino acid in the sequence
-      if (i < seq_length) {
-        # Trypsin does not cut if K or R is followed by Proline (P)
-        if (substr(seq_in, i + 1, i + 1) != "P") {
-          # If it's a cleavage site, extract the peptide
-          peptide <- substr(seq_in, current_peptide_start, i)
-          if(nchar(peptide) > 5 & nchar(peptide) < 13) {
-            peptides <- c(peptides, peptide)
-          }
-          
-          # Update the start of the next peptide
-          current_peptide_start <- i + 1
+                # Trypsin does not cut if K or R is followed by Proline (P)
+                if (next_aa != "P") {
+                    # If it's a cleavage site, extract the peptide
+                    peptide <- substr(seq_in, current_peptide_start, i)
+                    peptide_nostar <- str_replace_all(peptide, "\\*", "")
+                    print(peptide)
+                    print(nchar(peptide_nostar))
+                    if(nchar(peptide_nostar) > 5 & nchar(peptide_nostar) < 17) {
+                        peptides <- c(peptides, peptide)
+                    }
+                    
+                    # Update the start of the next peptide
+                    current_peptide_start <- i + 1
+                }
+            } else {
+                # If K or R is the last amino acid, it's a cleavage site (no P to follow)
+                peptide <- substr(seq_in, current_peptide_start, i)
+                peptide_nostar <- str_replace_all(peptide, "\\*", "")
+                print(peptide)
+                print(nchar(peptide_nostar))
+                if(nchar(peptide_nostar) > 5 & nchar(peptide_nostar) < 17) {
+                    peptides <- c(peptides, peptide)
+                }
+                
+                # Update the start for the next (non-existent) peptide
+                current_peptide_start <- i + 1
+            }
         }
-      } else {
-        # If K or R is the last amino acid, it's a cleavage site (no P to follow)
-        peptide <- substr(seq_in, current_peptide_start, i)
-        peptides <- c(peptides, peptide)
-        
-        # Update the start for the next (non-existent) peptide
-        current_peptide_start <- i + 1
-      }
     }
-  }
 
-  # Add the last peptide if there's any remaining sequence after the last cut
-  if (current_peptide_start <= seq_length) {
-    last_peptide <- substr(seq_in, current_peptide_start, seq_length)
-    peptides <- c(peptides, last_peptide)
-  }
+    # Add the last peptide if there's any remaining sequence after the last cut
+    if (current_peptide_start <= seq_length) {
+        last_peptide <- substr(seq_in, current_peptide_start, seq_length)
+        peptide_nostar <- str_replace_all(last_peptide, "\\*", "")
+        print(last_peptide)
+        print(nchar(peptide_nostar))
+        if(nchar(peptide_nostar) > 5 & nchar(peptide_nostar) < 17) {
+            peptides <- c(peptides, last_peptide)
+        }
+    }
 
-  return(peptides)
+    return(peptides)
 }
 
-tryptic_cut(prot_seq)
+pep_out <- trypsin_digest(prot_seq_mod)
+
+
+mouse_matrix = matrix(
+    c(253, 207, 291, 169), 
+    nrow = 2, 
+    byrow = TRUE, 
+    dimnames = list(
+        "Mouse" = c("OT1", "OT2"),
+        "autoimmunity" = c("Yes", "No")
+    )
+)
+
+print(fisher.test(mouse_matrix))
+
+
+
+
+ORC1 30585 30124 29821 30692 30206 30338 30367 29239 29877 31429 0.9262724
+ORC2 30375 29855 29886 29935 30907 30153 30544 29652 31211 30518 0.53
+ORC3 30285 29839 30229 30534 30162 30064 29983 29733 30512 30062 0.264176
+ORC4 29745 30397 30520 30090 29996 30143 30960 29658 29705 30448 0.9112587
+ORC5 30129 30246 29593 30449 29409 30223 31012 31251 30417 30756 0.08292843
+ORC6 29251 30330 30143 30694 29920 30170 30484 31098 30764 30568 0.0423089
+MCM2 29524 30399 30051 30668 29986 29489 29901 30555 30466 29861 0.6856001
+MCM3 30093 30462 30998 29851 29785 30247 30604 30632 30087 30402 0.3743958
+MCM4 30181 29617 29911 29592 29350 29801 31352 31012 30516 30307 0.06554106
+MCM5 30842 30514 30748 30556 30251 30424 30140 30811 30951 30204 0.6373761
+MCM6 29819 29419 29644 30174 30214 31230 30691 30472 30572 29934 0.07796511
+MCM7 30459 30365 29159 29883 29670 30362 30478 30353 30880 30083 0.1030876
+
+
+The above is a simulated experiment. I used the following R code to generate these results:
+
+set.seed(1000)
+generate_values <- function() {
+    trt <- rnorm(5, mean = 30000, sd = 500) |> round()
+    ctrl <- rnorm(5, mean = 30400, sd = 500) |> round()
+    return(list("trt" = trt, "ctrl" = ctrl))
+}
+
+cur_vals <- generate_values()
+print(paste0(
+    paste(cur_vals[["trt"]], collapse = " "), 
+    " ", 
+    paste(cur_vals[["ctrl"]], collapse = " "),
+    " ",
+    t.test(cur_vals[["trt"]], cur_vals[["ctrl"]], paired = TRUE)$p.value
+))
+
+
+print(paste0(
+    paste(cur_vals[["trt"]], collapse = " "), 
+    " ", 
+    paste(cur_vals[["ctrl"]], collapse = " ")
+))
+
+
+test_mat <- matrix(unlist(generate_values()), nrow = 1, byrow = TRUE)
+
+for (i in seq(1:11)) {
+    test_mat <- rbind(
+        test_mat, 
+        matrix(unlist(generate_values()), nrow = 1, byrow = TRUE)
+    )
+}
+
+rownames(test_mat) <- c(
+    "ORC1", 
+    "ORC2", 
+    "ORC3", 
+    "ORC4", 
+    "ORC5", 
+    "ORC6", 
+    "MCM2", 
+    "MCM3", 
+    "MCM4", 
+    "MCM5", 
+    "MCM6", 
+    "MCM7" 
+)
+
+scaled_mat <- t(scale(t(test_mat))) |> 
+    as.data.frame() |>
+    as.matrix()
+
+apply(scaled_mat, 2, sd)
+
+
+
